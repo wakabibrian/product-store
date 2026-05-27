0a5e0fba578b77a0479de8bb3c2126afa129ceb0
wakabibrian <wakabibrian95@gmail.com>
2026-05-28 01:36:29 +0300

    Feature: Added Navigation and Hero sections

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,7 +1,19 @@
 import "./Hero.css";
 
 function Hero() {
-  return <div>Hero</div>;
+  return (
+    <div className="hero-container">
+      <div className="hero-text-wrapper">
+        <h1>
+          Discover Amazing <span>Products</span>
+        </h1>
+        <p>
+          Explore trending products, premium collections, and everyday
+          essentials - all in one place.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
--- src/sections/Navigation/Navigation.jsx
+++ src/sections/Navigation/Navigation.jsx
@@ -1,7 +1,31 @@
 import "./Navigation.css";
 
 function Navigation() {
-  return <div>Navigation</div>;
+  return (
+    <nav>
+      <div className="navigation-container">
+        <div className="logo-wrapper">
+          <img
+            src="./assets/ShopNest-Logo.png"
+            alt="ShopNest Logo"
+            className="logo"
+          />
+        </div>
+
+        <ul className="nav-links-wrapper">
+          <li className="nav-link-item active">
+            <a>Products</a>
+          </li>
+          <li className="nav-link-item">
+            <a>Login</a>
+          </li>
+          <li className="nav-link-item">
+            <a>Signup</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default Navigation;
